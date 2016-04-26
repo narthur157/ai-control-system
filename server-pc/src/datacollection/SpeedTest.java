8e583e718fc0c71bf4fddcba8e1b9d9958ab6c70
narthur <narthur157@gmail.com>
2016-04-26 12:18:21 -0400

    Try doing a random test instead

--- server-pc/src/datacollection/SpeedTest.java
+++ server-pc/src/datacollection/SpeedTest.java
@@ -1,13 +1,16 @@
 package datacollection;
 
 import java.io.IOException;
+import java.util.Random;
 
 import communication.BrickComm;
 import communication.Command;
+
 import framework.Test;
 
 public class SpeedTest extends Test {
 		private int prevPower = 50;
+		private Random rand = new Random();
 		
 		public SpeedTest() throws IOException {
 			super();
@@ -17,13 +20,15 @@ public class SpeedTest extends Test {
 		public void test() {
 			int power;
 			
-			power = prevPower + 5;
-			
-			if (power > 100) power = 0;
-			
+			power = rand.nextInt(101);
+//			
+//			power = prevPower + 5;
+//			
+//			if (power > 100) power = 0;
+//			
 			BrickComm.sendCommand(Command.DISTURB_WHEEL, power); 
 			BrickComm.sendCommand(Command.CONTROL_WHEEL, power); 
-			
-			prevPower = power;
+//			
+//			prevPower = power;
 		}
 }
